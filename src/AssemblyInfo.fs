3f35a8a1f8c1afdc79f8283ebf11493315873da2
Team City <info@exira.com>
2016-01-02 22:04:21 +0000

    Bump version to 0.3.2

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -4,10 +4,10 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("ges-logger")>]
 [<assembly: AssemblyProductAttribute("Exira.EventStore.Logger")>]
 [<assembly: AssemblyDescriptionAttribute("Exira.EventStore.Logger is a Topshelf service to log EventStore events using Serilog")>]
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-[<assembly: AssemblyFileVersionAttribute("0.2.0")>]
-[<assembly: AssemblyMetadataAttribute("githash","835f8a808a4a9484255b9970823440fc2bc9420d")>]
+[<assembly: AssemblyVersionAttribute("0.3.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.2")>]
+[<assembly: AssemblyMetadataAttribute("githash","13041dd253ca9a460fdbab379b635cb6ac187e45")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.0"
+    let [<Literal>] Version = "0.3.2"
